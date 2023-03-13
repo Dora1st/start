8ebcb0e5b420b57c36f6bf15b09c7f466cef4c6c
Дарья <cold.dashytka@yandex.ru>
2023-03-13 19:31:26 +0100

    4rd version

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -1,5 +1,10 @@
 public class First {
     public static void main(String[] args) {
         System.out.println("123");
+        First first = new First();
+        System.out.println(first.plus(4,5));
+    }
+    public int plus(int a, int b){
+        return a+b;
     }
 }
