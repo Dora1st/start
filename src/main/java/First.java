817ff31274b91484d55012c5006ef8cd4b9603af
promark33 <promark33@gmail.com>
2023-03-13 20:40:08 +0100

    java 8 streams & lists example

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -1,9 +1,19 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class First {
     public static void main(String[] args) {
         System.out.println("123");
         //ghbjbjhbhgbhgbhgb
         First first = new First();
         System.out.println(first.plus(4,5));
+
+        List<String> list = Stream.of("12-31", "45-62", "78-91")
+                .map(s -> s.split("-")[0])
+                .collect(Collectors.toList());
+        System.out.println(list);
+
     }
     public int plus(int a, int b){
         return a+b;
